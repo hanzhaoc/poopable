52a88566daf2982255755556f9d4867a9d0cdd96
Hanzhao Chen <hanzhao.chen@appfolio.com>
2020-02-07 12:30:00 -0800

    hzc - initial app with code copyed from https://www.youtube.com/watch\?v\=KiBsVeT-Kqg

---
+++ README.md
@@ -0,0 +1,1 @@
+Start point of backend of poopible
---
+++ app.py
@@ -0,0 +1,71 @@
+import os
+import logging
+from flask import Flask
+from flask import request
+from slack import WebClient
+from slackeventsapi import SlackEventAdapter
+import ssl as ssl_lib
+import certifi
+from pprint import pprint
+from onboarding_tutorial import OnboardingTutorial
+
+# Initialize a Flask app to host the events adapter
+app = Flask(__name__)
+app.debug = True
+@app.route('/', methods=['POST'])
+def index():
+  pprint(request.form)
+  pprint(request.data)
+  return request.get_json()['challenge']
+
+slack_events_adapter = SlackEventAdapter(os.environ['SLACK_SIGNING_SECRET'], "/slack/events", app)
+
+# Initialize a Web API client
+slack_web_client = WebClient(token=os.environ['SLACK_BOT_TOKEN'])
+
+onboarding_tutorials_sent = {}
+
+def start_onboarding(user_id: str, channel: str):
+    # Create a new onboarding tutorial.
+    onboarding_tutorial = OnboardingTutorial(channel)
+
+    # Get the onboarding message payload
+    message = onboarding_tutorial.get_message_payload()
+
+    # Post the onboarding message in Slack
+    response = slack_web_client.chat_postMessage(**message)
+
+    # Capture the timestamp of the message we've just posted so
+    # we can use it to update the message after a user
+    # has completed an onboarding task.
+    onboarding_tutorial.timestamp = response["ts"]
+
+    # Store the message sent in onboarding_tutorials_sent
+    if channel not in onboarding_tutorials_sent:
+        onboarding_tutorials_sent[channel] = {}
+    onboarding_tutorials_sent[channel][user_id] = onboarding_tutorial
+
+    # ============== Message Events ============= #
+# When a user sends a DM, the event type will be 'message'.
+# Here we'll link the message callback to the 'message' event.
+@slack_events_adapter.on("message")
+def message(payload):
+    """Display the onboarding welcome message after receiving a message
+    that contains "start".
+    """
+    event = payload.get("event", {})
+
+    channel_id = event.get("channel")
+    user_id = event.get("user")
+    text = event.get("text")
+
+
+    if text and text.lower() == "start":
+        return start_onboarding(user_id, channel_id)
+
+if __name__ == "__main__":
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(logging.StreamHandler())
+    ssl_context = ssl_lib.create_default_context(cafile=certifi.where())
+    app.run(port=8000)
